ac21168361ba27bc76e9b37f631d37a1f8a26b92
Peter Byfield <peter.byfield@enable.com>
2019-11-28 12:29:07 +0000

    export widget from widget

--- src/widget.js
+++ src/widget.js
@@ -2,7 +2,7 @@ import { html, component, useState, useEffect } from 'haunted'
 
 const quoteURL = 'http://quotes.rest/qod.json?category=inspire'
 
-const Widget = () => {
+export const Widget = () => {
   const [quote, setQuote] = useState('')
   const [author, setAuthor] = useState('')
 
